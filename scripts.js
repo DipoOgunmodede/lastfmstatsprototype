75f08a5a9a0143eac257650e8220d68fa050e2dc
DipoOgunmodede <dipoogunmodede@gmail.com>
2019-05-21 23:29:48 +0100

    started adding a recent tracks promise request experimented with automatic refreshing

--- scripts.js
+++ scripts.js
@@ -9,33 +9,57 @@ new Vue({
     data() {
         return {
             myArtists: null,
-            cachedTimeStamp: {},
+            recentTracks: null,
+            cachedTopArtistsTimeStamp: {},
+            cachedRecentTracksTimeStamp: {},
             errorState: false,
-        cachedArtists: null
+            cachedArtists: null
 
         }
     },
-    mounted() {
-        getTopArtists: {
-            if (localStorage.cachedArtists) {
-              this.cachedArtists = true
-                this.myArtists = JSON.parse(localStorage.getItem('cachedArtists') || []);
-                this.cachedTimeStamp = JSON.parse(localStorage.getItem('cachedTimestamp') || [])
-            }
-
-        }
-    },
+    mounted() {},
     methods: {
         updateTopArtists: function(e) {
             axios
-                .get("https://ws.audioscrobbler.com/2.0/?api_key=2c5c5c19e5d21ce9cf86b13712a1bbed&format=json&method=user.getTopArtists&user=El_Mayo&period=overall&limit=200")
+                .get("https://ws.audioscrobbler.com/2.0/?api_key=2c5c5c19e5d21ce9cf86b13712a1bbed&format=json&method=user.getTopArtists&user=El_Mayo&period=overall&limit=50") //can I grab a .val() from the markup and splice it into the limit?
                 .then(response => this.myArtists = response.data.topartists.artist)
                 .then(response => localStorage.setItem('cachedArtists', JSON.stringify(this.myArtists)))
-                .then(response => this.cachedTimeStamp = { time: new Date().toLocaleString() }).then(response => localStorage.setItem('cachedTimestamp', JSON.stringify(this.cachedTimeStamp)))
+                .then(response => this.cachedTopArtistsTimeStamp = { time: new Date().toLocaleString() })
+                .then(response => localStorage.setItem('cachedTopArtistsTimeStamp', JSON.stringify(this.cachedTopArtistsTimeStamp)))
                 .catch((error) => {
-                  this.errorState= true;
-                    console.log(error.config)
+                    this.errorState = true;
+                    if (localStorage.cachedArtists) {
+                        this.cachedArtists = true
+                        this.myArtists = JSON.parse(localStorage.getItem('cachedArtists') || []);
+                        this.cachedTopArtistsTimeStamp = JSON.parse(localStorage.getItem('cachedTopArtistsTimeStamp') || [])
+                    }
                 });
+        },
+        updateRecentTracks: function(e) {
+   //setInterval is NOT a good idea here
+                axios
+                    .get("https://ws.audioscrobbler.com/2.0/?api_key=2c5c5c19e5d21ce9cf86b13712a1bbed&format=json&method=user.getrecenttracks&user=El_Mayo&period=overall&limit=100")
+                    .then(response => this.recentTracks = response.data.recenttracks.track)
+                    .then(response => localStorage.setItem('cachedRecentTracks', JSON.stringify(this.recentTracks)))
+                    .then(response => console.log(this.recentTracks['@attr'].find(status => nowplaying === true)))
+                    .then(response => this.cachedRecentTracksTimeStamp = { time: new Date().toLocaleString() })
+                    .then(response => localStorage.setItem('cachedRecentTracksTimeStamp', JSON.stringify(this.cachedRecentTracksTimeStamp)))
+                    .catch((error) => {
+                        this.errorState = true;
+                        if (localStorage.cachedRecentTracks) {
+                            this.cachedRecentTracks = true
+                            this.recentTracks = JSON.parse(localStorage.getItem('cachedRecentTracks') || [])
+                            this.cachedTopArtistsTimeStamp = JSON.parse(localStorage.getItem('cachedTopArtistsTimeStamp') || [])
+                        }
+                    });
+      
+
+        },
+        continuousMode: function(e) {
+            setInterval(function() {
+                updateRecentTracks();
+                updateTopArtists();
+            }, 5000); //work on getting this as a modifier to other methods
         }
     },
     computed: {
