3adba472961cfc48671dbae332ee80ae1eca68ed
felinesoft\dipo.ogunmodede <dipo.ogunmodede@DKTP-DEV-39>
2019-05-22 15:23:05 +0100

    fixed console error caused by trying to .find() nowplaying track

--- scripts.js
+++ scripts.js
@@ -36,23 +36,23 @@ new Vue({
                 });
         },
         updateRecentTracks: function(e) {
-   //setInterval is NOT a good idea here
-                axios
-                    .get("https://ws.audioscrobbler.com/2.0/?api_key=2c5c5c19e5d21ce9cf86b13712a1bbed&format=json&method=user.getrecenttracks&user=El_Mayo&period=overall&limit=100")
-                    .then(response => this.recentTracks = response.data.recenttracks.track)
-                    .then(response => localStorage.setItem('cachedRecentTracks', JSON.stringify(this.recentTracks)))
-                    .then(response => console.log(this.recentTracks['@attr'].find(status => nowplaying === true)))
-                    .then(response => this.cachedRecentTracksTimeStamp = { time: new Date().toLocaleString() })
-                    .then(response => localStorage.setItem('cachedRecentTracksTimeStamp', JSON.stringify(this.cachedRecentTracksTimeStamp)))
-                    .catch((error) => {
-                        this.errorState = true;
-                        if (localStorage.cachedRecentTracks) {
-                            this.cachedRecentTracks = true
-                            this.recentTracks = JSON.parse(localStorage.getItem('cachedRecentTracks') || [])
-                            this.cachedTopArtistsTimeStamp = JSON.parse(localStorage.getItem('cachedTopArtistsTimeStamp') || [])
-                        }
-                    });
-      
+            //setInterval is NOT a good idea here
+            axios
+                .get("https://ws.audioscrobbler.com/2.0/?api_key=2c5c5c19e5d21ce9cf86b13712a1bbed&format=json&method=user.getrecenttracks&user=El_Mayo&period=overall&limit=100")
+                .then(response => this.recentTracks = response.data.recenttracks.track)
+                .then(response => localStorage.setItem('cachedRecentTracks', JSON.stringify(this.recentTracks)))
+                .then(response => this.cachedRecentTracksTimeStamp = { time: new Date().toLocaleString() })
+                .then(response => localStorage.setItem('cachedRecentTracksTimeStamp', JSON.stringify(this.cachedRecentTracksTimeStamp)))
+                .catch((error) => {
+                    this.errorState = true;
+                    if (localStorage.cachedRecentTracks) {
+                        this.cachedRecentTracks = true
+                        this.recentTracks = JSON.parse(localStorage.getItem('cachedRecentTracks') || []);
+                        this.cachedRecentTracksTimeStamp = JSON.parse(localStorage.getItem('cachedRecentTracksTimeStamp') || [])
+
+                    }
+                });
+
 
         },
         continuousMode: function(e) {
